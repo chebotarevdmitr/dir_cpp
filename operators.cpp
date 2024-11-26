f44d86ed151460039d2b032d6fa6d35f9a4e9895
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2024-11-26 23:01:10 +0300

    operator+

---
+++ operators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+   cout << "\tDemonstration of arithmetic operators " << endl;
+   cout << "Enter two integer :" << endl;
+   int numb1 = 0, numb2 = 0;
+   cin >> numb1;
+   cin >> numb2;
+
+   cout << numb1 << " + " << numb2 << " = " << numb1 + numb2 << endl;
+   return 0;
+}
