3afb6c90ffe1fd725e34d9c52213b37a0696981e
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2024-11-27 23:06:34 +0300

    initialization option

--- increment.cpp
+++ increment.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
    cout << "Demonstration of increment " << endl;
-   int x, y;
-   x = 10;
-   y = ++x;
+   int x = 10 ,  y = ++x;
+   // x = 10;
+   //  y = ++x;
    cout << " x =10 ,y = ++x" << " y = " << y << endl;
    x = 10;
    y = x++;
